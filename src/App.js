789d5550cae4378ef05281e095ccb9caf180f71c
Sanika Rahate <sanikarahate2003@gmail.com>
2023-10-22 14:52:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Calculator } from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+    <div  style={{ backgroundColor:"grey"}}  >
+      <h2 style={{color:"black"}}>React-task-5</h2>
+      <Calculator />
     </div>
+    </center>
   );
 }
 
